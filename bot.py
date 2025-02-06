6354000a1fca061be8122bc0823cd4c97af8e02b
Danny Christian Rojas Garcia <e_2021101385B@uncp.edu.pe>
2025-02-06 15:22:15 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,25 @@
-import os
 import telebot
+import threading
+import time
 
-TOKEN = os.getenv("TOKEN")
+# 🔥 Tu token de Telegram
+TOKEN = "7373395275:AAHWtC-ssrB4GhDGQAiQMk7D41ywNpCrijI"
 bot = telebot.TeleBot(TOKEN)
+
+# ID del usuario al que se enviarán mensajes periódicos
+CHAT_ID = 1666690040
+
+# Función para enviar mensajes cada 30 segundos
+def keep_alive():
+    while True:
+        try:
+            bot.send_message(CHAT_ID, "Manteniendo el bot activo en Render 🚀")
+        except Exception as e:
+            print(f"Error al enviar mensaje: {e}")
+        time.sleep(30)  # Espera 30 segundos antes de enviar el siguiente mensaje
+
+# Inicia el proceso en segundo plano
+threading.Thread(target=keep_alive, daemon=True).start()
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
@@ -14,5 +31,6 @@ def echo_all(message):
 
 print("Bot corriendo...")
 bot.polling()
+
 
 
